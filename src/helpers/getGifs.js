540f60345fc9b30e7d9b4ee5d95d05aab84d1c21
ajgp8821 <ajgp8821@gmail.com>
2022-04-19 09:40:06 -0300

    1st functional part

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,20 @@
+
+export const getGift = async ( category) => {
+
+    const apiKey = 'xkZMmVXrMhZcXmWHlBwm8LjbCop6P3xo';
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=${apiKey}`;
+
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+    
+    const gifs = data.map( img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    });
+    // console.log(gifs);
+    // setImages( gifs );
+    return gifs;
+}
---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import { getGift } from '../helpers/getGifs';
+
+export const useFetchGifs = ( category ) => {
+
+    const [state, setState] = useState({
+        data: [],
+        loading: true
+    });
+
+    useEffect( () => {
+        getGift( category )
+            .then( imgs => {
+
+                setState({
+                    data: imgs,
+                    loading: false
+                });
+            })
+    }, [ category ]);
+
+    return state; // { data: [], loading: true }
+
+}
+
